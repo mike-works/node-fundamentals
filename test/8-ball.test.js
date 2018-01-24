98641cd82fe7b4e4965ee68aa6bca75e00bb3c92
Michael North <michael.l.north@gmail.com>
2018-01-23 16:26:34 -0800

    Initial commit

---
+++ test/8-ball.test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+describe('Exercise 1: Magic 8-ball', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
